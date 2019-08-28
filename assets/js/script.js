1732778fe0334f5c95ba21e2ff875701027684ab
Cameron Lattz <camerolattz@gmail.com>
2019-08-28 11:20:35 -0500

    testing fitbit login

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,8 +42,20 @@ const script = function () {
         });
     }
 
+    const _test = function () {
+        // get the url 
+        var url = window.location.href;
+        //getting the access token from url 
+        var access_token = url.split("#")[1].split("=")[1].split("&")[0];
+        // get the userid 
+        var userId = url.split("#")[1].split("=")[2].split("&")[0];
+        console.log(access_token);
+        console.log(userId);
+    }
+
     document.addEventListener("DOMContentLoaded", function () {
         _initBulma();
+        _test();
         document.getElementById("form").addEventListener("submit", _searchRequest);
     });
 }();
